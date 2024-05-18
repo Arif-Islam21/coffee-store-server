85149b42e30d73b5f133e64d2a21c29e248df730
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-05-18 15:41:40 +0530

    fetching an error for not useing await

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ async function run() {
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log(user);
-      const result = userCollection.insertOne(user);
+      const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
